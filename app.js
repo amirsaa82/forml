81829202bff85e3e688c1d4cf75e860d37747baa
amirsaa82 <amirkish82@yahoo.com>
2024-01-06 14:34:02 +0330

    added files

---
+++ app.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById("btn")
+let name = document.getElementById("name")
+let price = document.getElementById("price")
+let model = document.getElementById("model")
+let col = document.querySelector(".col-2")
+let btns = document.querySelectorAll("button")
+
+btn.addEventListener("click", e=>{
+    e.preventDefault()
+    sendData()
+})
+
+
+function sendData(){
+    productData = {
+        productModel : model.value,
+        productPrice : price.value,
+    }
+
+    fetch('https://infinityshop-1-default-rtdb.firebaseio.com/products.json', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(productData)
+    })
+    .then(res => {
+        console.log(res);
+        clearData()
+    })
+    .catch(err => console.log(err))
+
+   
+}
+
+function clearData(){
+    name.value = ""
+    price.value = ""
+}
+
+
